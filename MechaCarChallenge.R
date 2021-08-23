d4e324a6e9709a4942ba9f61602142a274994e07
Wendy Mudzinski <wendymud@gmail.com>
2021-08-22 20:26:35 -0700

    final update of R file

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -2,8 +2,8 @@ MechaCarChallenge.R
 library(dplyr)
 # Linear Regression to Predict MPG
 # read csv to dataframe
-mechacar_mpg <- read.csv(file='./resources/MechaCar_mpg.csv', check.names = F, stringsAsFactors = F)
-head(mechacar_mpg_df)
+mechacar_mpg <- read.csv(file='MechaCar_mpg.csv', check.names = F, stringsAsFactors = F)
+head(mechacar_mpg)
 # multiple linear regression
 lm(mpg~vehicle_length+vehicle_weight+spoiler_angle+ground_clearance+AWD, data = mechacar_mpg)
 # find the p-value and r-squared
@@ -14,9 +14,9 @@ suspension_coil <- read.csv(file = 'Suspension_Coil.csv', check.names = F, strin
 head(suspension_coil)
 
 # create a summary dataframe
-total_summary <- suspension_coil %>% summarize(Mean = mean(PSI), Median = median(PSI), Variance = var(PSI), SD = sd(PSI))
+summarize_demo <- suspension_coil %>% summarize(Mean = mean(PSI), Median = median(PSI), Variance = var(PSI), SD = sd(PSI))
 # create summaries for each lot
-lot_summary <- suspension_coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean = mean(PSI), Median = median(PSI), Variance = var(PSI), SD = sd(PSI), .groups = 'keep')
+lot_demo <- suspension_coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean = mean(PSI), Median = median(PSI), Variance = var(PSI), SD = sd(PSI), .groups = 'keep')
 
 # T-tests on suspension coils
 t.test(suspension_coil$PSI, mu=1500)
